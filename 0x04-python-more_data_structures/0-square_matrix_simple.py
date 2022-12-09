9a06452b1cb910be38fdaec370359efa9e069c32
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-12-09 07:50:17 -0800

    hff

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3 
-def square_matrix_simple(matrix=[]): 
-    return [[y*y for y in x] for x in matrix]
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    return [[y*y for y in x] for x in matrix]
