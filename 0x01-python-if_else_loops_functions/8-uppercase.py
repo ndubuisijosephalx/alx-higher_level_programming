91160d4ada1ac084208de49482c673d7ae73af29
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-12-11 11:25:30 -0800

    hfggh

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
-d
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        num = ord(x)
+        if (num >= 97 and num <= 122):
+            upper = chr((num - 97) + 65)
+        else:
+            upper = x
+        print("{}".format(upper), end="")
+    print("")
